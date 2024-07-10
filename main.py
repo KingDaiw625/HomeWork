649dbd78175afcdca22814678e85dce5a15700d5
KingDaiw625 <mikhail_maksimov_2004@mail.ru>
2024-07-10 20:15:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+#1st program
+print(9 ** 0.5 * 5)
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+#3rd program
+print((1234 % 1004 // 10) + (5678 % 5008 // 10))
+#4th program
+print(13 == 13 or 42 == 42 or int(13.42,42.13))
